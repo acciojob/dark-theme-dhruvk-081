ebe5a5615537491d1ba674135adf801f014150c6
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-01-12 07:13:01 +0000

    Update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+let swap = document.getElementById("swap");
+swap.onclick = function () {
+    document.body.classList.toggle("night");
+    if(document.body.classList.toggle("night")){
+        document.button.classList.toggle("button-night");
+    }
+}
 //your code here
 
