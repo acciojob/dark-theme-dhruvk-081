af7a74d66ddadd714e304785d8de90e593e68d0a
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-01-12 08:33:30 +0000

    Update-2

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let swap = document.getElementById("swap");
-swap.onclick = function () {
-    document.body.classList.toggle("night");
-    if(document.body.classList.toggle("night")){
-        document.button.classList.toggle("button-night");
-    }
-}
-//your code here
+let swapTheme = document.getElementById("swap");
+let buttonTheme = document.querySelector("#swap");
 
+
+swapTheme.onclick = function () {
+    document.body.classList.toggle("night");
+    buttonTheme.classList.toggle("button_night");
+}
